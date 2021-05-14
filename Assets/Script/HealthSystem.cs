c5ea7bdd0d934f524d01fba1e7dd3445e01c9b0a
PREMINDA <premindabandara0777@gmail.com>
2021-05-15 01:39:51 +0530

    Health system of buildings

---
+++ Assets/Script/HealthSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField] private int healtAmountMax;
+    private int healthAmount;
+
+    private void Awake()
+    {
+        healthAmount = healtAmountMax;
+    }
+    public void Damage(int damageAmount)
+    {
+        healthAmount -= damageAmount;
+        healthAmount = Mathf.Clamp(healthAmount,0,healtAmountMax);
+    }
+    public bool IsDead()
+    {
+        return healthAmount == 0;
+    }
+    public int  GetHealthAmount()
+    {
+        return healthAmount;
+    }
+}
