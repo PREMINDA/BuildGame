c917000d36c2f54e61799b3298ebd6932618992d
PREMINDA <premindabandara0777@gmail.com>
2021-05-17 01:09:12 +0530

    Enemy Script

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]private Transform transformHq;
+    private Rigidbody2D rigidbody2d;
+    private float speed = 6f;
+    private void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector3 ang = (transformHq.position - transform.position).normalized;
+        rigidbody2d.velocity = ang * speed;
+    }
+}
