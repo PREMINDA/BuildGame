867e56ac94b19e6a27f4c3a5c2453a75dbbdc9cd
PREMINDA <premindabandara0777@gmail.com>
2021-05-18 01:27:42 +0530

    target for new closer targets

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField]private Transform transformHq;
+    private Transform targetTransform;
     private Rigidbody2D rigidbody2d;
     private float speed = 6f;
+ 
     private void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        targetTransform = transformHq;
     }
 
     void Update()
     {
         Vector3 ang = (transformHq.position - transform.position).normalized;
         rigidbody2d.velocity = ang * speed;
-        if (transformHq == null) {
-            Destroy(this.gameObject);
-        }
-        Debug.Log(transformHq);
+        LookForNewTarget();
+
+
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -31,5 +34,33 @@ public class Enemy : MonoBehaviour
             Destroy(this.gameObject);
         }
         
+    }
+    private void LookForNewTarget()
+    {
+        float tartgetMaxRadius = 10f;
+
+        Collider2D[] collider2DArray = Physics2D.OverlapCircleAll(transform.position, tartgetMaxRadius);
+        
+        foreach(Collider2D collider2d in collider2DArray)
+        {
+            Buildings buildings = collider2d.GetComponent<Buildings>();
+            
+            if (buildings != null) {
+                Debug.Log("......................." + buildings);
+                if (targetTransform == null)
+                {
+                    targetTransform = buildings.transform;
+                }
+                else
+                {
+                    if(Vector3.Distance(transform.position,buildings.transform.position)
+                        < Vector3.Distance(transform.position, transformHq.position))
+                    {
+                        targetTransform = buildings.transform;
+                    }
+                }
+            }
+            
+        }
     }
 }
